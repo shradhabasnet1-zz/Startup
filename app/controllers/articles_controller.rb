266737d482080eb1044d4351dd092d7348348f71
shradhabasnet1 <shradha.basnet@cloudfactory.com>
2018-02-02 14:03:28 +0545

    Initialize app.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,26 @@
 class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def index
+        @articles = Article.all
+    end
+    
     def new
     end
 
     def create
-        render plain: params[:article].inspect
+        @article =  Article.new(params.require(:article).permit(:title,:text))
+
+        @article.save
+        redirect_to @article
     end
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
+    
+
 end
